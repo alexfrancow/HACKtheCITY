8dc12ec1c7874260ec48b2696ffed006e72b12e5
root <root@debian>
2019-08-14 00:36:56 +0200

    containers

--- app/mods/mod_main/views.py
+++ app/mods/mod_main/views.py
@@ -4,6 +4,8 @@ from app.lib.hcdb import create_hackthecity_db, check_volume_path, select_databa
 import time
 import json
 import os
+import subprocess
+import socket
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
 app = Flask(__name__)
@@ -26,24 +28,41 @@ def choose_db():
     return render_template('choose_db.html', dbs=dbs)
 
 
+@main_blueprint.route('/use_db', methods= ['GET', 'POST'])
+def use_db():
+    db = request.form['city']
+    db = db.replace(".db", "")
+    volume = check_volume_path(db)
+    volume = ''.join(volume)
+    volume = subprocess.getoutput("pwd")+"/"+volume
+    os.system("cp compose.yml docker-compose.ymll")
+    fin = open("docker-compose.ymll", "rt")
+    fout = open("docker-compose.yml", "wt")
+    for line in fin:
+        fout.write(line.replace('$VOLUME', volume))
+    fin.close()
+    fout.close()
+    os.system("rm docker-compose.ymll")
+    os.system("sysctl -w vm.max_map_count=262144")
+    os.system("chmod -R 777 volumes")
+    os.system("docker-compose up")
+    time.sleep(30)
+
+    return render_template('dashboard.html', db=db)
+
+@main_blueprint.route('/create_db', methods= ['GET', 'POST'])
+def create_db():
+    return render_template('index.html')
+
+
 @main_blueprint.route('/setup', methods=['POST'])
 def setup():
-    time.sleep(30)
+    time.sleep(20)
     if request.method == 'POST':
         city = request.form['city']
-        print(city)
         create_hackthecity_db(city)
 
-        if request.method == 'radio1':
-            zabbix_scripts = request.form['']
-        if request.method == ' radio2':
-            openvas_ip = request.form['']
-        if request.method == 'radio3':
-            openvas_ips = request.form['']
-        if request.method == 'radio4':
-            print(1)
-
-    return Response("hola", mimetype='text/html')
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug= True)
